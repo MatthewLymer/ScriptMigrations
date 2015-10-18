de7e677b478209aac555808f0a3fa5acd5807bbb
Matthew Lymer <matthew.lymer@outlook.com>
2015-10-18 15:37:41 -0400

    Update build script

--- Build/default.ps1
+++ Build/default.ps1
@@ -5,6 +5,54 @@
 	$robocopyPath = 'C:\Windows\System32\robocopy.exe'
 }
 
+task Default -Depends RunTests, Coalesce
+
+task Coalesce -Depends CompileApp {
+	$artifactPath = Join-Path (OutputDirectory) 'Artifact'
+
+	function Robocopy-Project($projectName) {
+		$src = Join-Path $outputDirectory $projectName
+
+		exec {
+			&$robocopyPath $src $artifactPath '/E'
+
+			if ($global:LastExitCode -Lt 8) {
+				$global:LastExitCode = 0
+			}
+		}
+	}
+	
+	Robocopy-Project 'MigratorConsole'
+	Robocopy-Project 'SqlServerMigrator' 
+}
+
+task RunTests -Depends CompileTests {
+	Run-Tests 'Migrator.Tests'
+	Run-Tests 'MigratorConsole.Tests'
+	Run-Tests 'SqlServerMigrator.Tests'
+	Run-Tests 'Migrator.Shared.Tests'
+}
+
+task CompileApp -Depends NugetPackageRestore {
+	Build-Project 'MigratorConsole'
+	Build-Project 'SqlServerMigrator'
+}
+
+task CompileTests -Depends NugetPackageRestore {
+	Build-Project 'Migrator.Tests'
+	Build-Project 'MigratorConsole.Tests'
+	Build-Project 'SqlServerMigrator.Tests'
+	Build-Project 'Migrator.Shared.Tests'
+}
+
+task NugetPackageRestore {
+	$solutionPath = Join-Path (SolutionDirectory) "ScriptMigrations.sln"
+	
+	exec {
+		.\nuget.exe restore $solutionPath
+	}
+}
+
 function Normalize-Path($path) {
 	if ($path.StartsWith('\')) {
 		$path = $pwd.Drive.Name + ':' + $path
@@ -39,54 +87,4 @@ function OutputDirectory {
 
 function SolutionDirectory {
 	Normalize-Path $solutionDirectory
-}
-
-task Default -Depends RunTests, Coalesce
-
-task Coalesce -Depends CompileApp {
-	$artifactPath = Join-Path (OutputDirectory) 'Artifact'
-
-	function Robocopy-Project($projectName) {
-		$src = Join-Path $outputDirectory $projectName
-
-		exec {
-			&$robocopyPath $src $artifactPath '/E'
-
-			if ($global:LastExitCode -Lt 8) {
-				$global:LastExitCode = 0
-			}
-		}
-	}
-	
-	Robocopy-Project 'Migrator'
-	Robocopy-Project 'MigratorConsole'
-	Robocopy-Project 'SqlServerMigrator' 
-}
-
-task RunTests -Depends CompileTests {
-	Run-Tests 'Migrator.Tests'
-	Run-Tests 'MigratorConsole.Tests'
-	Run-Tests 'SqlServerMigrator.Tests'
-}
-
-task CompileApp -Depends NugetPackageRestore {
-	Build-Project 'Migrator'
-	Build-Project 'MigratorConsole'
-	Build-Project 'SqlServerMigrator'
-}
-
-task CompileTests -Depends NugetPackageRestore {
-	Build-Project 'Migrator.Tests'
-	Build-Project 'MigratorConsole.Tests'
-	Build-Project 'SqlServerMigrator.Tests'
-}
-
-task NugetPackageRestore {
-	$solutionPath = Join-Path (SolutionDirectory) "ScriptMigrations.sln"
-
-	Write-Host $solutionPath
-
-	exec {
-		.\nuget.exe restore $solutionPath
-	}
 }
